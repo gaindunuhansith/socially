1d825ef739cbcad5eed19a4c7608c8faa05fc71a
gaindunuhansith <136603140+gaindunuhansith@users.noreply.github.com>
2025-07-18 14:12:19 +0530

    feat/add dark mode

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+import ModeToggle from "@/components/ui/ModeToggle";
 import { SignedIn, SignedOut, SignInButton, SignOutButton, SignUpButton, UserButton, UserProfile } from "@clerk/nextjs";
 import Image from "next/image";
 
@@ -5,13 +7,18 @@ export default function Home() {
   return (
     <div>
       <SignedOut>
-        <SignInButton mode="modal" />
+        <SignInButton mode="modal" >
+          <Button>Sign In</Button>
+        </SignInButton>
       </SignedOut>
 
       <SignedIn>
         <UserButton />
         <SignOutButton />
       </SignedIn>
+
+      <ModeToggle />
+      
 
 
     </div>
