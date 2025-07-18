20945862a6002d055dae4b75263e59887c8b9cde
gaindunuhansith <136603140+gaindunuhansith@users.noreply.github.com>
2025-07-18 11:35:14 +0530

    feat/configure clerk authentication

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,19 @@
+import { SignedIn, SignedOut, SignInButton, SignOutButton, SignUpButton, UserButton, UserProfile } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Hello World</div>
+    <div>
+      <SignedOut>
+        <SignInButton mode="modal" />
+      </SignedOut>
+
+      <SignedIn>
+        <UserButton />
+        <SignOutButton />
+      </SignedIn>
+
+
+    </div>
   );
 }
